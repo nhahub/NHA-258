7497fc881cfb84f4af02b5553a2b3034bff7bb8b
hageranter <138611708+hageranter@users.noreply.github.com>
2025-11-15 20:29:46 +0200

    Create the firsy UI Pages with its models

---
+++ SmartTransportation.BLL/Pages/index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text;
+using System.Security.Claims;
+
+
+public sealed class IndexModel : PageModel
+{
+
+}
