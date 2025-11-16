f7a87358ee2510a529db84c6a58d7a3af3eeb5dc
USSIF <cds.YoussefEhab24713@alexu.edu.eg>
2025-11-16 23:42:29 +0200

    update

--- SmartTransportation.Web/Program.cs
+++ SmartTransportation.Web/Program.cs
@@ -2,7 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddHttpClient();
 
 
 var app = builder.Build();
