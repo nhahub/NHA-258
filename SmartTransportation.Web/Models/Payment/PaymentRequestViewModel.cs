2a598215a88570e0196a2a594868162c876a1b8e
USSIF <cds.YoussefEhab24713@alexu.edu.eg>
2025-11-17 12:33:11 +0200

    remove controller and views

--- SmartTransportation.Web/Models/Payment/PaymentRequestViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SmartTransportation.Web.Models.Payment
-{
-    public class PaymentRequestViewModel
-    {
-        public int BookingId { get; set; }
-        public string PaymentMethodId { get; set; } = default!;
-    }
-}
